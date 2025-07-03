4ea0e69979c4fb9e28ce1619ad3c7f53843837e6
CaptaiRich <ahiakpormaxwell32@gmail.com>
2025-07-03 12:06:15 +0000

    Count Queries2

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -27,3 +27,23 @@ GROUP BY
     p.property_id, p.name
 ORDER BY 
     rank;
+
+
+
+SELECT 
+    property_id,
+    property_name,
+    total_bookings,
+    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS row_number
+FROM (
+    SELECT 
+        p.property_id,
+        p.name AS property_name,
+        COUNT(b.booking_id) AS total_bookings
+    FROM 
+        Property p
+    LEFT JOIN 
+        Booking b ON p.property_id = b.property_id
+    GROUP BY 
+        p.property_id, p.name
+) AS ranked_properties;
